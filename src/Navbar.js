8b93c8ae35d3ac97fb4fa3037d33507d35ea22c1
James Volmert <james.volmert@gmail.com>
2020-03-04 16:54:03 -0700

    added snackbar and styled select

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import Slider from 'rc-slider';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 import 'rc-slider/assets/index.css';
 import './Navbar.css';
 
@@ -10,16 +13,26 @@ class Navbar extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            format: 'hex'
+            format: 'hex',
+            open: false
         }
         this.handleChange = this.handleChange.bind(this);
+        this.closeSnackbar = this.closeSnackbar.bind(this);
     }
 
     handleChange(e) {
         this.setState({
-            format: e.target.value
+            format: e.target.value,
+            open: true
         });
         this.props.handleChange(e.target.value);
+    }
+
+    closeSnackbar() {
+        this.setState({
+            open: false
+        });
+
     }
 
     render() {
@@ -30,7 +43,8 @@ class Navbar extends Component {
         } = this.props;
 
         const {
-            format
+            format,
+            open
         } = this.state;
 
         return (
@@ -60,6 +74,27 @@ class Navbar extends Component {
                         <MenuItem value="rgba">RGBA - rgba(0,0,0,0.5)</MenuItem>
                     </Select>
                 </div>
+                <Snackbar
+                    anchorOrigin={{vertical: 'bottom', horizontal: 'left'}}
+                    open={open}
+                    onClose={this.closeSnackbar}
+                    autoHideDuration={5000}
+                    message={<span id="message-content">Format Changed: {format.toUpperCase()} </span>}
+                    ContentProps={{
+                        'aria-describedby': "message-content",
+                        "role": "alert"
+                    }}
+                    action={[
+                        <IconButton 
+                            onClick={ this.closeSnackbar } 
+                            key="close" 
+                            color="inherit" 
+                            aria-label="close"
+                            >
+                            <CloseIcon />
+                        </IconButton>
+                    ]}
+                />
             </nav>
         )
     }
