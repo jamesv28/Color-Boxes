993cf7eaf8fcdfa77282a61c9ae70a1ec8fed34d
James Volmert <james.volmert@gmail.com>
2020-03-03 17:09:18 -0700

    added rc-slider to the palette

--- src/Palette.js
+++ src/Palette.js
@@ -1,19 +1,46 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
 import './Palette.css';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 class Palette extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            level: 500
+        }
+        this.changeLevel = this.changeLevel.bind(this);
+    }
+
+    changeLevel(level) {
+        this.setState({
+            level
+        })
+    }
+
     render() {
-        const colorBoxes = this.props.colors.map((color, idx) => (
+        const {
+            level
+        } = this.state;
+
+        const colorBoxes = this.props.palette.colors[this.state.level].map((color, idx) => (
             <ColorBox 
-                background={color.color} 
+                background={color.hex} 
                 name={color.name}
                 key={idx}
             />
         ));
         return (
             <div className="Palette">
+                <Slider 
+                    step={100}
+                    defaultValue={level} 
+                    min={100}
+                    max={900}
+                    onAfterChange={this.changeLevel}
+                />
                 <div className="Palette-colors">
                     {colorBoxes}
                 </div>
